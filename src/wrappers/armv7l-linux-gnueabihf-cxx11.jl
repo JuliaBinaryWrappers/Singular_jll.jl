d03dc7eaf4ff946dbf22f1763b343ac87397762c
jlbuild <juliabuildbot@gmail.com>
2021-07-28 10:12:08 +0000

    Singular_jll build 402.100.100+0

--- src/wrappers/armv7l-linux-gnueabihf-cxx11.jl
+++ src/wrappers/armv7l-linux-gnueabihf-cxx11.jl
@@ -7,12 +7,12 @@ using GMP_jll
 using MPFR_jll
 JLLWrappers.@generate_wrapper_header("Singular")
 JLLWrappers.@declare_executable_product(Singular)
-JLLWrappers.@declare_library_product(libfactory, "libfactory-4.2.0.so")
+JLLWrappers.@declare_library_product(libfactory, "libfactory-4.2.1.so")
 JLLWrappers.@declare_library_product(libomalloc, "libomalloc-0.9.6.so")
 JLLWrappers.@declare_executable_product(libparse)
-JLLWrappers.@declare_library_product(libpolys, "libpolys-4.2.0.so")
-JLLWrappers.@declare_library_product(libsingular, "libSingular-4.2.0.so")
-JLLWrappers.@declare_library_product(libsingular_resources, "libsingular_resources-4.2.0.so")
+JLLWrappers.@declare_library_product(libpolys, "libpolys-4.2.1.so")
+JLLWrappers.@declare_library_product(libsingular, "libSingular-4.2.1.so")
+JLLWrappers.@declare_library_product(libsingular_resources, "libsingular_resources-4.2.1.so")
 function __init__()
     JLLWrappers.@generate_init_header(cddlib_jll, FLINT_jll, GMP_jll, MPFR_jll)
     JLLWrappers.@init_executable_product(
